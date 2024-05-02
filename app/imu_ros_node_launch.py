e90f652b580052ad60ad476ce69a1cb2cb69ac46
bernardolanza93 <bernardo.lanza.tech@gmail.com>
2024-05-02 12:09:45 +0200

    sh exec imu

--- app/imu_ros_node_launch.py
+++ app/imu_ros_node_launch.py
@@ -8,25 +8,12 @@ script1 = cartella + "ros_imu_config.sh"
 script2 = cartella + "ros_node_pub.sh"
 
 # Avvia entrambi gli script Bash contemporaneamente
-process1 = subprocess.Popen(["bash", script1], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-process2 = subprocess.Popen(["bash", script2], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+process1 = subprocess.Popen(["bash", script1])
+process2 = subprocess.Popen(["bash", script2])
 
-# Leggi e stampa l'output continuo dello script 1
-print("Output continuo dello script 1:")
-for line in iter(process1.stdout.readline, ''):
-    print(line.strip())
-    if process1.poll() is not None:
-        break
 
-# Leggi e stampa l'output continuo dello script 2
-print("\nOutput continuo dello script 2:")
-for line in iter(process2.stdout.readline, ''):
-    print(line.strip())
-    if process2.poll() is not None:
-        break
-
-# Attendere che entrambi i processi completino l'esecuzione
+# Attendi che entrambi i processi completino l'esecuzione
 process1.wait()
 process2.wait()
 
-print("\nEntrambi gli script sono stati eseguiti.")
+print("Entrambi gli script sono stati eseguiti.")
