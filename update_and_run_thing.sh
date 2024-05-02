5245b1d54d42c16149d75abb9d628383068b3394
bernardolanza93 <bernardo.lanza.tech@gmail.com>
2024-05-02 14:45:09 +0200

    sh exec imu

--- update_and_run_thing.sh
+++ update_and_run_thing.sh
@@ -6,5 +6,10 @@ echo "UPDATED"
 
 sleep 2
 
-cd app/
-python3 imu_ros_node_launch.py
+
+cd
+
+cd catkin_ws/
+
+source devel/setup.bash
+roslaunch xsens_mti_driver /home/usuario/JETSON_AQUIRE_LIDAR_GNSS_IMU/xsens_mti_node_custom.launch
