0e105a79442205e20a07c17b84367a26fe31714d
bernardolanza93 <bernardo.lanza.tech@gmail.com>
2024-05-02 12:43:03 +0200

    sh exec imu

--- app/imu_ros_node_launch.py
+++ app/imu_ros_node_launch.py
@@ -8,12 +8,14 @@ script1 = cartella + "ros_imu_config.sh"
 script2 = cartella + "ros_node_pub.sh"
 
 # Avvia entrambi gli script Bash contemporaneamente
-process1 = subprocess.Popen(["bash", script1])
-process2 = subprocess.Popen(["bash", script2])
+process1 = subprocess.Popen(["bash", script1], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+process2 = subprocess.Popen(["bash", script2], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-
-# Attendi che entrambi i processi completino l'esecuzione
-process1.wait()
-process2.wait()
-
-print("Entrambi gli script sono stati eseguiti.")
+# Crea un terzo processo per salvare l'output del secondo processo su file
+with open("output_script2.txt", "w") as output_file:
+    # Leggi l'output del secondo processo e scrivilo su file
+    for line in iter(process2.stdout.readline, ''):
+        output_file.write(line)
+        print(line.strip())  # Stampa l'output per vedere in tempo reale
+        if process2.poll() is not None:
+            break
