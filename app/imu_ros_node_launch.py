87b2b5ff2ce429127ddcb4243806e7f4dcf1aa19
bernardolanza93 <bernardo.lanza.tech@gmail.com>
2024-05-02 12:07:24 +0200

    sh exec imu

--- app/imu_ros_node_launch.py
+++ app/imu_ros_node_launch.py
@@ -8,14 +8,23 @@ script1 = cartella + "ros_imu_config.sh"
 script2 = cartella + "ros_node_pub.sh"
 
 # Avvia entrambi gli script Bash contemporaneamente
-process1 = subprocess.Popen(["bash", script1])
-process2 = subprocess.Popen(["bash", script2])
+process1 = subprocess.Popen(["bash", script1], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+process2 = subprocess.Popen(["bash", script2], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
+# Ottieni l'output e l'output di errore dello script 1
+output_script1, error_script1 = process1.communicate()
+print("Output dello script 1:")
+print(output_script1)
 
-# Attendi che entrambi i processi completino l'esecuzione
-process1.wait()
+# Ottieni l'output e l'output di errore dello script 2
+output_script2, error_script2 = process2.communicate()
+print("\nOutput dello script 2:")
+print(output_script2)
 
-
-process2.wait()
-
-print("Entrambi gli script sono stati eseguiti.")
+# Stampa eventuali errori
+if error_script1:
+    print("\nErrore nello script 1:")
+    print(error_script1)
+if error_script2:
+    print("\nErrore nello script 2:")
+    print(error_script2)
